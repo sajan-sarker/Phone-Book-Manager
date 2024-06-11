6851229a042986e6d9e49f0cedacd80eeb7f8c07
Sajan Kumer Sarker <91801349+sajan-sarker@users.noreply.github.com>
2024-06-11 15:56:36 +0000

    add data fnc

--- phone_book_manager.py
+++ phone_book_manager.py
@@ -1,7 +1,22 @@
 import csv
+import sys
 
 def add_data():
-    print("Execute add data function.")
+    # data adding function
+    fname= input("Enter first name: ")
+    lname= input("Enter last name: ")
+    cell = input("Enter number: ")
+    email = input("Enter email address:")
+    address = input("Enter address: ")
+    fieldname = ['serial', 'first name', 'last name', 'cell', 'email address', 'address']
+    serial = 0
+    with open("contact.csv", 'a', newline="") as file:
+        writer = csv.DictWriter(file, fieldnames= fieldname)
+        if cell == True:
+            serial = serial +1
+        writer.writerows({'serial': serial, 'first name': fname, 'last name': lname, 'cell': cell, 'email address': email, 'address': address})
+
+
 
 def search_data():
     print("Execute add search function.")
@@ -23,21 +38,23 @@ def main():
     print("3. Update data?")
     print("4. Delete data?")
     print("5. View data?")
-
-    interupt = int(input("What you want to do? (1/2/3/4): "))
-    match interupt:
-        case 1:
-            add_data()
-        case 2:
-            search_data()
-        case 3:
-            update_data()
-        case 4:
-            delete_data()
-        case 5:
-            print_data()
-        case _:
-            print("Wrong entry: Try again!")
+    while True:
+        interupt = int(input("What you want to do? (1/2/3/4): "))
+        match interupt:
+            case 1:
+                add_data()
+            case 2:
+                search_data()
+            case 3:
+                update_data()
+            case 4:
+                delete_data()
+            case 5:
+                print_data()
+            case 6:
+                sys.exit()
+            case _:
+                print("Wrong entry: Try again!")
 
 
 
